fdd8ffe97f52dbfd2dc880e29ebaa9d7c70a2bdb
yoyo150696 <70186612+yoyo150696@users.noreply.github.com>
2022-12-30 10:17:00 +0200

    Update GroupAdmin.java

--- src/main/java/GroupAdmin.java
+++ src/main/java/GroupAdmin.java
@@ -12,48 +12,32 @@ public class GroupAdmin implements Sender {
     @Override
     public void register(Member obj) {
         this.mems.add(obj);
+        obj.update(us);
     }
 
     @Override
     public void unregister(Member obj) {
         this.mems.remove(obj);
-
     }
-
+//
     @Override
     public void insert(int offset, String obj) {
         us.insert(offset,obj);
-        for (int i=0;i<mems.size();i++){
-            mems.get(i).update(us);
-        }
     }
 //
     @Override
     public void append(String obj) {
         us.append(obj);
-        for (int i=0;i<mems.size();i++){
-            mems.get(i).update(us);
-        }
-
     }
 
     @Override
     public void delete(int start, int end) {
         us.delete(start, end);
-        for (int i = 0; i < mems.size(); i++) {
-            mems.get(i).update(us);
-        }
     }
 
     @Override
     public void undo() {
             us.undo();
-            for (int i=0;i<mems.size();i++){
-                mems.get(i).update(us);
-        }
-
-
-
     }
 
     public UndoableStringBuilder getUdoableStringBuilder() {
